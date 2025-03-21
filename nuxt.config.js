1eef60307a7b3f50ee520aa385b6e99c794be5e0
foamiebear <foamiebear@gmail.com>
2025-03-21 11:58:40 +0800

    refactor: 替换Bootstrap为Element UI并更新相关配置
    将Bootstrap及其相关依赖替换为Element UI，并更新了`nuxt.config.js`中的CSS和插件配置。同时移除了`package-lock.json`的忽略规则，并更新了部分依赖版本。

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,10 +18,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'element-ui/lib/theme-chalk/index.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/element-ui'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -33,11 +35,18 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
   ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [/^element-ui/],
   }
 }
